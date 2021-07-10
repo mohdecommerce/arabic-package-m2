0705333e1eaf7a44a3e52cb437ccfa42d1d963a2
Mohammed Ibrahim <85409119+mohdecommerce@users.noreply.github.com>
2021-07-10 06:16:31 +0300

    Add files via upload

---
+++ Magar/ar_SA/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::LANGUAGE, 'magar_ar_sa', __DIR__);
